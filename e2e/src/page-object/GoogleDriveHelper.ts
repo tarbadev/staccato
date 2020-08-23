b81ac69029c07c12e876209133b7d324d0dbfe06
Tarbadev <tarbadev@gmail.com>
2020-08-22 18:28:58 -0700

    Extract list files in GoogleDriveHelper

--- e2e/src/page-object/GoogleDriveHelper.ts
+++ e2e/src/page-object/GoogleDriveHelper.ts
@@ -12,11 +12,7 @@ const drive = google.drive({
   }),
 })
 
-// drive.files.list({ fields: 'files(id, name)' }).then(response => console.log(response.data))
-export const listPermissions = () => drive.permissions.list({
-  fileId: '1LuiJSfvrrmTN2Mn0222ArqLQJz8jY-iw',
-  fields: 'permissions(id, emailAddress)',
-}).then(response => console.log(response.data))
+let mainFolderId: string
 
 const findFolder = (name: string): Promise<string> => {
   return drive.files.list({
@@ -25,8 +21,22 @@ const findFolder = (name: string): Promise<string> => {
   }).then((response: GaxiosResponse) => response.data.files[0].id)
 }
 
+const getMainFolderId = async (): Promise<string> => {
+  if (mainFolderId){
+    return mainFolderId
+  } else {
+    return await findFolder('Staccato')
+  }
+}
+
+export const listFiles = () => drive.files.list({ fields: 'files(id, name)' }).then(response => console.log(response.data))
+export const listPermissions = async () => drive.permissions.list({
+  fileId: await getMainFolderId(),
+  fields: 'permissions(id, emailAddress)',
+}).then(response => console.log(response.data))
+
 export const createFolder = async (name: string): Promise<string | null | undefined> => {
-  const folderId = await findFolder('Staccato')
+  const folderId = await getMainFolderId()
   return drive.files.create({
     requestBody: {
       name,
@@ -47,7 +57,7 @@ export const deleteFolder = async (name: string): GaxiosPromise<void> => {
 }
 
 export const deletePermission = async (email: string, failOnError = false): Promise<void> => {
-  const folderId = await findFolder('Staccato')
+  const folderId = await getMainFolderId()
   const permissions = await drive.permissions.list({ fileId: folderId, fields: 'permissions(id, emailAddress)' })
     .then(response => response.data.permissions)
   // @ts-ignore

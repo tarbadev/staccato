a4d70fb38fb46dcdfd79c9df4b214fba38fcb5a4
Tarbadev <tarbadev@gmail.com>
2021-01-16 21:53:21 -0800

    Extract database plugin

---
+++ cypress/plugins/database.plugin.ts
@@ -0,0 +1,48 @@
+import e2eDbCredentials from '@config/e2e.database.config'
+import { ConnectionOptions, createConnection, getConnection } from 'typeorm'
+
+export default {
+  'database:createConnection'(): Promise<null> {
+    return createConnection(e2eDbCredentials as ConnectionOptions)
+      .then(() => null)
+      .catch(err => {
+        cy.log('An error happened while connecting to DB', err.message)
+        return null
+      })
+  },
+
+  'database:closeConnection'(): Promise<null> {
+    return getConnection().close()
+      .then(() => null)
+      .catch(err => {
+        cy.log('An error happened while closing the DB', err.message)
+        return null
+      })
+  },
+
+  async 'database:clear'(): Promise<null> {
+    const conn = getConnection()
+    const tables = [
+      'resource_author',
+      'author',
+      'resource',
+      'bundle',
+    ]
+
+    for (const table of tables) {
+      await conn.query(`DELETE FROM ${table}`)
+    }
+
+    return null
+  },
+
+  'database:store'<T>({ entity, object }: { entity: string; object: T }): Promise<T | null> {
+    return getConnection()
+      .getRepository(entity)
+      .save(object)
+      .catch(err => {
+        cy.log('An error happened while storing the DB', err.message)
+        return null
+      })
+  },
+}
--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,5 +1,4 @@
-import { ConnectionOptions, createConnection, getConnection } from 'typeorm'
-import e2eDbCredentials from '@config/e2e.database.config'
+import databasePlugin from './database.plugin'
 import PluginEvents = Cypress.PluginEvents
 import ConfigOptions = Cypress.ConfigOptions
 
@@ -7,49 +6,5 @@ import ConfigOptions = Cypress.ConfigOptions
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on: PluginEvents): void | ConfigOptions | Promise<ConfigOptions> => {
-  on('task', {
-    'database:createConnection'(): Promise<null> {
-      return createConnection(e2eDbCredentials as ConnectionOptions)
-        .then(() => null)
-        .catch(err => {
-          cy.log('An error happened while connecting to DB', err.message)
-          return null
-        })
-    },
-
-    'database:closeConnection'(): Promise<null> {
-      return getConnection().close()
-        .then(() => null)
-        .catch(err => {
-          cy.log('An error happened while closing the DB', err.message)
-          return null
-        })
-    },
-
-    async 'database:clear'(): Promise<null> {
-      const conn = getConnection()
-      const tables = [
-        'resource_author',
-        'author',
-        'resource',
-        'bundle',
-      ]
-
-      for (const table of tables) {
-        await conn.query(`DELETE FROM ${table}`)
-      }
-
-      return null
-    },
-
-    'database:store'<T>({ entity, object }: { entity: string; object: T }): Promise<T | null> {
-      return getConnection()
-        .getRepository(entity)
-        .save(object)
-        .catch(err => {
-          cy.log('An error happened while storing the DB', err.message)
-          return null
-        })
-    },
-  })
+  on('task', databasePlugin)
 }
